8a08f2b72dafcba08474d566a09bfc6807bcb13f
Brian Baggs <brianbaggs@hotmail.com>
2026-09-16 18:34:33 -0300

    Update blink clip downloader to 6.0.3

--- blink_clip_downloader/frontend/src/components/layout/AppSidebar.vue
+++ blink_clip_downloader/frontend/src/components/layout/AppSidebar.vue
@@ -387,7 +387,7 @@ onUnmounted(() => {
       v-model:visible="showAbout"
       modal
       dismissable-mask
-      header="About Blink Clips 6.0.2"
+      header="About Blink Clips 6.0.3"
       :style="{ width: '26rem' }"
       :draggable="false"
     >
